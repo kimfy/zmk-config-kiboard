b3eb4e95e8b9a0086633883c3cca1052baabd7df
kimfy <kim@wwan.no>
2022-08-23 23:40:46 +0200

    Add norwegian remapping

---
+++ config/boards/shields/kiboard/norwegian-keymap.c
@@ -0,0 +1,10 @@
+/*
+ * Since ZMK uses US ANSI, we need to remap their keys to norwegian.
+ * To do this, change the keyboard layout on your PC to US and press
+ * the key you want to re-map. Whatever is output is what you need to remap.
+ */
+
+#define NB_AT N2       // @
+#define NB_LBRC N9     // (
+#define NB_RBRC N8     // )
+
