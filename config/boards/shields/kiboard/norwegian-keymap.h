ce99d7c38497b6a816238099ee6908fa43e187ca
Kim Iversen <kim@wwan.no>
2022-08-24 22:42:22 +0200

    More norwegian mappings

--- config/boards/shields/kiboard/norwegian-keymap.h
+++ config/boards/shields/kiboard/norwegian-keymap.h
@@ -4,6 +4,18 @@
  * the key you want to re-map. Whatever is output is what you need to remap.
  */
 
-#define NB_AT RA(NUMBER_2)       // @
-#define NB_LBRC LS(NUMBER_8)     // (
-#define NB_RBRC LS(NUMBER_9)     // )
+#define NB_AT   RA(N2)     // @
+#define NB_LBRC RA(N7)     // }
+#define NB_RBRC RA(N0)     // }
+#define NB_LPAR LS(N8)     // (
+#define NB_RPAR LS(N9)     // )
+#define NB_LPAR RA(N8)     // [
+#define NB_RPAR RA(N9)     // ]
+
+#define NB_PRCNT LS(N5)    // %
+#define NB_DLLR  RA(N4)    // $
+#define NB_CARET LS(N6)    // ^
+#define NB_TILDE LS(GRAVE) // ~
+#define NB_GRAVE PIPE      // `
+#define NB_PIPE LS(BSLH)   // |
+
